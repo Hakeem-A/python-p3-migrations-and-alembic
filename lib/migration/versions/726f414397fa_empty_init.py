72608886001ff2eddcfb184a6fa26dc0f7cf2195
Abdi Mohamed <bashanesix@gmail.com>
2025-03-08 14:41:59 +0300

    lab done

---
+++ lib/migration/versions/726f414397fa_empty_init.py
@@ -0,0 +1,24 @@
+"""Empty init
+
+Revision ID: 726f414397fa
+Revises: 
+Create Date: 2025-03-08 14:21:42.815999
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '726f414397fa'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
